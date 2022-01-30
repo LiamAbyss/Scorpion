b7af1cb8d57c5104e3f8c33aa3cfc7a55facbfc7
LiamAbyss <46683039+LiamAbyss@users.noreply.github.com>
2022-01-30 23:45:15 +0100

    Squashed commit of the following:
    commit a3b9fbb6a0a16fac54f9eb22aed5660020ef5a9b
    Author: RemiArbache <32673658+RemiArbache@users.noreply.github.com>
    Date:   Sun Jan 30 18:41:20 2022 +0100
    
        Refactor details view
    
    commit 4a260596a88ff46e392572cb3b2f0a3f08ed8f2b
    Author: RemiArbache <32673658+RemiArbache@users.noreply.github.com>
    Date:   Sat Jan 29 13:46:03 2022 +0100
    
        Refactor course details view
    
    commit 4886075e6c7920dc3904d051f190db7011e07a2e
    Author: RemiArbache <32673658+RemiArbache@users.noreply.github.com>
    Date:   Sat Jan 29 13:45:13 2022 +0100
    
        Refactor course details view
    
    commit 8513a7ad705c1b3a869ed5a2d1704e5c5132e55c
    Author: RemiArbache <32673658+RemiArbache@users.noreply.github.com>
    Date:   Sat Jan 29 13:44:39 2022 +0100
    
        Swap last and first name
    
    commit 618416573c740fcdfb0370bceacd4dc79ba5e82f
    Author: RemiArbache <32673658+RemiArbache@users.noreply.github.com>
    Date:   Sat Jan 29 13:44:05 2022 +0100
    
        Add close icon

--- app/src/main/java/fr/yncrea/scorpion/model/Person.java
+++ app/src/main/java/fr/yncrea/scorpion/model/Person.java
@@ -5,4 +5,13 @@ public class Person {
     public String firstName;
 
     public String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+    }
+
 }
